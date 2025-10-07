9b477e77facaedb1feedbcd8a2a7a01cde608083
CyberNerd <futurinov809@gmail.com>
2025-10-07 15:21:32 +0000

    all bug is resolved

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -20,7 +20,7 @@ val localProps = localProperties()
 
 android {
     namespace = "com.example.locacharge"
-    compileSdk = flutter.compileSdkVersion
+    compileSdk = 36
     ndkVersion = "27.0.12077973"
 
     compileOptions {
